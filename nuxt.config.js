02b42581732dc66dc70afd7c3b7313d948fb69f7
DaksinWorld <onosov20071@gmail.com>
2021-12-10 18:49:42 +0200

    new commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Do you want to order a website? Expert website design and development worldwide from Onosov Business!Fair prices. IT-Services' },
+      { hid: 'description', name: 'description', content: 'Do you want to order a website? Expert website design and development worldwide from Onosov Business!Fair prices. IT-Services. Service' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
