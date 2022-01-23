223a9d22aa1e63120fb8db8a0ee5bae6ea1a54d1
Danya Onosov <72867805+DaksinWorld@users.noreply.github.com>
2022-01-23 20:52:32 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Do you want to order a website? Expert website design and development worldwide from Onosov Business!Fair prices. IT-Services. Service' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'freelancehunt', content: '137dc61b05e986a'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
